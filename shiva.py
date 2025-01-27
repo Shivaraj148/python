6d5ccf813e6ce4e0fdf29942d69508ddfbb236cb
Shivaraj148 <sonayak.cs@gmail.com>
2025-01-27 14:40:58 +0530

    Add files via upload
    This is python file

---
+++ shiva.py
@@ -0,0 +1,11 @@
+x=5
+if x==5:
+ print("terminal")
+ 
+ x=int(input("give some number"))
+ if x>0:
+  print("the number is positive")
+ elif x<0:
+  print("number is negative")
+ else:
+  print("The number is zero")
