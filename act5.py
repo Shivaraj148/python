9e61ee3eb6e8da6898e32294328c5f4b2c283b8c
Shivaraj148 <sonayak.cs@gmail.com>
2025-02-19 15:28:27 +0530

    act5.py

--- act5.py
+++ act5.py
@@ -1,5 +1,5 @@
 
-5. rows = int(input("Enter the number of rows :"))
+ rows = int(input("Enter the number of rows :"))
     for i in range(1,rows + 1):
         print(" " * (rows-i) + " ".join(str(num)  for num in range(1,i + 1)))              
 
