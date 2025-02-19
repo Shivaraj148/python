3e7ecca66f1324c04bcfef6b4b300ca42a62d751
Shivaraj148 <sonayak.cs@gmail.com>
2025-02-19 15:27:44 +0530

    act4.py

--- act4.py
+++ act4.py
@@ -1,4 +1,4 @@
-4.rows = int(input("Enter the number of rows'))
+  rows = int(input("Enter the number of rows'))
     for i in range(1,rows + 1):
         print(" " * (rows-1) + " ".join(num)  for rows in range(1,i + 1)))
 
